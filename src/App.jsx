59345fcdeafc966434693d32c65b537dfcaaf15e
JayroGT <jayro.esp.sg@gmail.com>
2024-07-08 20:36:22 +0800

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Chart from './component/Chart'
 import { ExampleTwo } from "./component/ExampleTwo";
 import { ExampleThree } from "./component/ExampleThree";
+import { Mix } from "./component/Mix";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/example_1" element={<Chart/>} /> 
         <Route path="/example_2" element={<ExampleTwo/>} />
         <Route path="/example_3" element={<ExampleThree/>} />
+        <Route path="/mix" element={<Mix/>} />
       </Routes>
     </Router>
     </>
